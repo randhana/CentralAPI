8996f38201b295085e8611084a9712aced50ac8e
randhana <pulathisi1921@gmail.com>
2024-07-18 16:00:17 +0530

    Modified rateLimit() method to return false when rate limit is exceeded.

--- application/helpers/RateLimiter.php
+++ application/helpers/RateLimiter.php
@@ -4,15 +4,18 @@ class RateLimiter {
     public static function rateLimit($ipAddress, $maxRequestsPerMinute) {
         global $redis;
 
-        $requestCountKey = 'request_count_' . $ipAddress;
+        $requestCountKey = 'api_request_' . $ipAddress;
         $requestCount = (int) $redis->get($requestCountKey);
 
         if ($requestCount >= $maxRequestsPerMinute) {
             ResponseHelper::sendResponse(429, ['error' => 'Rate limit exceeded']);
+            return false;
         }
 
         $redis->incr($requestCountKey);
         $redis->expire($requestCountKey, 60);
+        
+        return true;
     }
 }
 ?>
