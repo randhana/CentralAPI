3107f6817e55e43c45653b5b469ebf7a3ca4e6d2
randhana <pulathisi1921@gmail.com>
2024-07-19 16:31:23 +0530

    Add logging for outgoing responses

--- application/helpers/ResponseHelper.php
+++ application/helpers/ResponseHelper.php
@@ -1,15 +1,24 @@
 <?php
 
 class ResponseHelper {
+    private static $logger;
+
+    public static function setLogger($logger) {
+        self::$logger = $logger;
+    }
+
     public static function sendResponse($status, $data) {
-        global $log; 
+        if (self::$logger === null) {
+            throw new Exception('Logger not set');
+        }
+        
         http_response_code($status);
         header('Content-Type: application/json');
         $response = json_encode($data);
         echo $response;
-        
+
         // Log the response
-        $log->info('Outgoing response', [
+        self::$logger->info('Outgoing response', [
             'status' => $status,
             'response' => $response
         ]);
