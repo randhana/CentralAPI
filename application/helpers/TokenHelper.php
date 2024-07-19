c4c0f878661841949eee3012945b7a3972c08e3f
randhana <pulathisi1921@gmail.com>
2024-07-19 16:31:03 +0530

    Add logging for token verification

--- application/helpers/TokenHelper.php
+++ application/helpers/TokenHelper.php
@@ -5,7 +5,12 @@ use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key;
 
 class TokenHelper {
-    private static $publicKeyPath = './public.pem'; //public key 
+    private static $publicKeyPath = './public.pem'; // public key
+    private static $logger;
+
+    public static function initializeLogger($logger) {
+        self::$logger = $logger;
+    }
 
     public static function getBearerToken() {
         $headers = apache_request_headers();
@@ -19,7 +24,9 @@ class TokenHelper {
     }
 
     public static function verifyToken($token) {
-        global $log; // global log object
+        if (!self::$logger) {
+            throw new \Exception('Logger is not initialized.');
+        }
 
         try {
             $publicKey = file_get_contents(self::$publicKeyPath);
@@ -27,18 +34,18 @@ class TokenHelper {
             $user = (array) $decoded;
 
             if (new DateTime() > new DateTime('@' . $user['exp'])) {
-                $log->warning('Token has expired', ['token' => $token]);
+                self::$logger->warning('Token has expired', ['token' => $token]);
                 ResponseHelper::sendResponse(401, ['error' => 'Token has expired']);
                 return false;
             }
 
             return $user;
         } catch (\Firebase\JWT\ExpiredException $e) {
-            $log->warning('Token has expired', ['token' => $token]);
+            self::$logger->warning('Token has expired', ['token' => $token, 'error' => $e->getMessage()]);
             ResponseHelper::sendResponse(401, ['error' => 'Token has expired']);
             return false;
         } catch (\Exception $e) {
-            $log->warning('Invalid token', ['token' => $token, 'error' => $e->getMessage()]);
+            self::$logger->warning('Invalid token', ['token' => $token, 'error' => $e->getMessage()]);
             ResponseHelper::sendResponse(401, ['error' => 'Invalid token: ' . $e->getMessage()]);
             return false;
         }
