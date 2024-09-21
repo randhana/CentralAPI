2df1eb235b00ecbca51cf3e2a14ec9dd07b69e2a
Pulathisi Kariyawasam <pulathisi1921@gmail.com>
2024-09-22 01:37:18 +0530

    set default datetimezone to Asia/Colombo

--- application/logger.php
+++ application/logger.php
@@ -6,10 +6,12 @@ use Monolog\Handler\StreamHandler;
 function initializeLoggers() {
     //request logs
     $requestLogger = new Logger('request_logger');
+    $requestLogger->setTimezone(new \DateTimeZone('Asia/Colombo'));
     $requestLogger->pushHandler(new StreamHandler('../logs/requests.log', Logger::INFO));
 
     //error logs
     $errorLogger = new Logger('error_logger');
+    $requestLogger->setTimezone(new \DateTimeZone('Asia/Colombo'));
     $errorLogger->pushHandler(new StreamHandler('../logs/errors.log', Logger::ERROR));
 
     return [
